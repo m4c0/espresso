a020e3fdabb9f73b104dbdb416c07465714a5f61
m4c0 <me@m4c0.com>
2016-06-05 18:30:50 +0100

    more basic int return tests

--- tests/ClassParser/JIT/005-return-int-const.cpp
+++ tests/ClassParser/JIT/005-return-int-const.cpp
@@ -2,17 +2,27 @@
 
 using namespace Espresso::ClassParser;
 
-int main() {
+static bool _test(const char * data, int size, int result) {
     auto jit = JIT()
-        .dataStream(DataStream("\x05\xac", 2)) // iconst_2; ireturn
+        .dataStream(DataStream(data, size))
         .returnType(JIT::Int);
-    if (!jit) return 1;
+    if (!jit) return false;
 
     int (*fn)() = (int(*)())jit.buildFunction();
-    if (!fn) return 2;
+    if (!fn) return false;
 
-    if (fn() != 2) return 3;
+    if (fn() != result) return false;
 
-    return 0;
+    return true;
+}
+
+int main() {
+    if (!_test("\x02\xac", 2, -1)) return 1; // iconst_m1; ireturn
+    if (!_test("\x03\xac", 2,  0)) return 2; // iconst_0;  ireturn
+    if (!_test("\x04\xac", 2,  1)) return 3; // iconst_1;  ireturn
+    if (!_test("\x05\xac", 2,  2)) return 4; // iconst_2;  ireturn
+    if (!_test("\x06\xac", 2,  3)) return 5; // iconst_3;  ireturn
+    if (!_test("\x07\xac", 2,  4)) return 6; // iconst_4;  ireturn
+    if (!_test("\x08\xac", 2,  5)) return 7; // iconst_5;  ireturn
 }
 
